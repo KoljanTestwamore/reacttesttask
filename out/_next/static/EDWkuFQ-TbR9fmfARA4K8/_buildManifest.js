7b13bbdc8874558b22ec3b10127b26f1f9f27e7d
KoljanTestwamore <anton.pab@gmail.com>
2020-11-11 11:18:44 +0300

    Deployment errors fixes

--- out/_next/static/EDWkuFQ-TbR9fmfARA4K8/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002Feb8c6418bdcf69ef7bec.css","static\u002Fchunks\u002Fpages\u002Findex-fed025c2a96791320cab.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-cff691ed6419045f5861.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
